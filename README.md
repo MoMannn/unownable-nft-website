85131ad8e357952be5abd1f4c7080b28c9ac659c
Tadej Vengust <vengust.tadej@gmail.com>
2023-12-28 12:47:38 +0100

    Add page design

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # unownable-nft-website
 
 Simple website to display unownable NFT and take possession of it.
+
+apillon hosting deploy-website ./dist --uuid 7b5a5654-5184-472e-a6a4-8086d4bb3a68 --key 88797e6c-fc47-4bd5-a1db-0c35144873c6 --secret qsf4neJuVRGM
---
+++ logic.js
@@ -0,0 +1,80 @@
+let provider = null;
+const nftAddress = "0x45cFFcC1dE8cb677D54B496168152473A4de8873";
+const chainId = "0x13881";
+let wallet = null;
+
+$(function () {
+  setInterval(checkPossesion, 5000);
+});
+
+async function checkPossesion() {
+  if (provider) {
+    await loadNFT();
+  }
+}
+
+async function connect() {
+  let currentChain = null;
+  $("#connectError").html("");
+
+  try {
+    initProvider();
+    currentChain = await getCurrentChain();
+  } catch (e) {
+    $("#connectError").html("Metamask not supported");
+    return;
+  }
+
+  if (currentChain != chainId) {
+    try {
+      await switchChain();
+      initProvider();
+      currentChain = await getCurrentChain();
+    } catch (e) {
+      $("#connectError").html("Metamask not supported");
+      return;
+    }
+  }
+
+  await ethereum.request({ method: "eth_requestAccounts" });
+  wallet = await provider.getSigner().getAddress();
+
+  $("#connect").html(`<p>${wallet}</p>`);
+
+  await loadNFT();
+}
+
+async function switchChain() {
+  await ethereum.request({
+    method: "wallet_switchEthereumChain",
+    params: [{ chainId }],
+  });
+}
+
+async function getCurrentChain() {
+  return ethereum.request({ method: "eth_chainId" });
+}
+
+async function initProvider() {
+  provider = new ethers.providers.Web3Provider(window.ethereum);
+}
+
+async function loadNFT() {
+  const contract = new ethers.Contract(nftAddress, nftAbi, provider);
+
+  const owner = await contract.ownerOf(1);
+  $("#nftInfo").html(`<p>Currently in possession of:</p><p>${owner}</p>`);
+}
+
+async function takePossession() {
+  if (!provider) {
+    await connect();
+  }
+  const nft = new ethers.Contract(nftAddress, nftAbi, provider).connect(
+    provider.getSigner()
+  );
+  const tx = await nft.takePossession();
+  // $("#nftInfo").html(
+  //   `<p>Transaction in progress:</p><a target="_blank" href=${tx}>See on polygonscan</a>`
+  // );
+}
